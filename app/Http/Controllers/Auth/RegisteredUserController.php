3e3acc9ae41fadcc622fcd3a645d41ac1aaaa940
Asey <103695527+aseylais@users.noreply.github.com>
2023-04-07 15:41:57 +0200

    07/04-2nd correction

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,7 +35,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255','unique:'.Compte::class],
+            'name' => ['required', 'string', 'max:255',
             'email' => ['required','email', 'max:255' , 'unique:'.Compte::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
